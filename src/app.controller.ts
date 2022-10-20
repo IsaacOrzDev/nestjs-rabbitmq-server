5035f4f50a71a69f406860e02aafb1ca6f90d33c
Isaac <isaachkdeveloper@gmail.com>
2022-10-21 00:07:33 +0800

    add mq

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'greeting' })
+  getGreetingMessage(name: string): string {
+    return `Hello ${name}`;
+  }
+
+  @MessagePattern({ cmd: 'greeting-async' })
+  async getGreetingMessageAsync(name: string): Promise<string> {
+    return `Hello ${name} Async`;
+  }
+
+  @EventPattern('book-created')
+  async handleBookCreatedEvent(data: Record<string, unknown>) {
+    console.log('data');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'books_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  app.listen();
 }
 bootstrap();
